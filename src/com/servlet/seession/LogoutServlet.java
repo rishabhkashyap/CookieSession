2563ac79c53ea0c17d754f65bbd57065d34d25c8
rishabhkashyap <kashyap.rishab@hotmail.com>
2016-02-23 01:17:21 +0530

    Commiting to github for first time

---
+++ src/com/servlet/seession/LogoutServlet.java
@@ -0,0 +1,67 @@
+package com.servlet.seession;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LogoutServlet
+ */
+@WebServlet(description = "LogoutServlet", urlPatterns = { "/LogoutServlet" })
+public class LogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public LogoutServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+		Cookie loginCookie = null;
+		Cookie[] cookies = request.getCookies();
+		if (cookies != null) {
+			for (Cookie cook : cookies) {
+				if (cook.getName().equals("user")) {
+					loginCookie = cook;
+					break;
+				}
+			}
+			loginCookie.setMaxAge(0);
+			response.setHeader("Cache-Control",
+					"no-cache, no-store, must-revalidate"); // HTTP 1.1.
+			response.setHeader("Pragma", "no-cache"); // HTTP 1.0.
+			response.setDateHeader("Expires", 0); // Proxies.
+			RequestDispatcher disp = request.getRequestDispatcher("index.jsp");
+			out.println("You have been successfully logged out");
+			disp.include(request, response);
+		}
+
+	}
+
+}
---
+++ src/com/servlet/session/LoginServlet.java
@@ -0,0 +1,54 @@
+package com.servlet.session;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet(description = "LoginServlet", urlPatterns = { "/LoginServlet" })
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+		protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+			String username=request.getParameter("user");
+			String password= request.getParameter("pwd");
+			if((username.equals("abc"))&&(password.equals("abc"))){
+				Cookie cookie=new Cookie("user",username);
+				
+				//Cookie will expire in 30 mins
+				cookie.setMaxAge(30*60);
+				response.addCookie(cookie);
+				response.sendRedirect("LoginSuccess.jsp");	
+				
+			}else{
+				RequestDispatcher disp=request.getRequestDispatcher("index.jsp");
+				PrintWriter out=response.getWriter();
+				out.println("<h1>Wrong user name or password</h1>");
+			    disp.forward(request, response);
+			}
+			
+			
+	}
+
+}
