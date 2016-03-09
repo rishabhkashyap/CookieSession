444db7d4a6aec3682fef341f684c551ec2a86792
rishabhkashyap <kashyap.rishab@hotmail.com>
2016-03-09 07:23:54 +0530

    commit

--- src/com/servlet/session/LoginServlet.java
+++ src/com/servlet/session/LoginServlet.java
@@ -17,38 +17,39 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(description = "LoginServlet", urlPatterns = { "/LoginServlet" })
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-		protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-			String username=request.getParameter("user");
-			String password= request.getParameter("pwd");
-			if((username.equals("abc"))&&(password.equals("abc"))){
-				Cookie cookie=new Cookie("user",username);
-				
-				//Cookie will expire in 30 mins
-				cookie.setMaxAge(30*60);
-				response.addCookie(cookie);
-				response.sendRedirect("LoginSuccess.jsp");	
-				
-			}else{
-				RequestDispatcher disp=request.getRequestDispatcher("index.jsp");
-				PrintWriter out=response.getWriter();
-				out.println("<h1>Wrong user name or password</h1>");
-			    disp.forward(request, response);
-			}
-			
-			
+	public LoginServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		String username = request.getParameter("user");
+		String password = request.getParameter("pwd");
+		if ((username.equals("abc")) && (password.equals("abc"))) {
+			Cookie cookie = new Cookie("user", username);
+
+			// Cookie will expire in 30 mins
+			cookie.setMaxAge(30 * 60);
+			response.addCookie(cookie);
+			response.sendRedirect("LoginSuccess.jsp");
+
+		} else {
+			RequestDispatcher disp = request.getRequestDispatcher("index.jsp");
+			PrintWriter out = response.getWriter();
+			out.println("<h1>username or password is wrong...Pleae try again!!<h1>");
+			disp.include(request, response);
+		}
+
 	}
 
 }
